7f5c5799344b8dafbea050bd0484c441b67273c4
atrox3d <atrox3d@gmail.com>
2018-07-13 15:49:05 +0000

    setup

---
+++ 0x06/fakeclip.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def copy():
+	print("copy")
+
+def paste():
+	print("paste")
+	
+if __name__ == '__main__':
+	print("you shoul import this file")
---
+++ 0x06/main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+import fakeclip
+
+print("first test")
+
+
