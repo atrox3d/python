bc08645fb38d384ba8b8005ec392b0187db7356e
atrox3d <atrox3d@gmail.com>
2018-07-13 17:03:53 +0000

    paths playing

--- 0x06/main.py
+++ 0x06/main.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
+import sys
+from pathlib import Path 
+
+# just to try the complete syntax
 from fakepyperclip import fakepyperclip as pyperclip
 
-print("first test")
+# log-like
+print("[{}] first test".format(Path(sys.argv[0]).name))
 
-help(pyperclip)
+#help(pyperclip)
+print(pyperclip.__doc__)
 
 pyperclip.copy()
 pyperclip.paste()
