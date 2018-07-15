edfd60bd077c2dde9821cbbe2cc08c76b2e1c0c4
atrox3d <atrox3d@gmail.com>
2018-07-15 10:45:25 +0200

    logging part 1

--- 0x06/testclip.py
+++ 0x06/testclip.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
+import logging
 import sys
 from pathlib import Path 
 
 # just to try the complete syntax
 from fakepyperclip import fakepyperclip as pyperclip
 
-# log-like
-print("[{}] first test".format(Path(sys.argv[0]).name))
+logging.basicConfig(
+    #filename="test.log",
+    level=logging.DEBUG,
+    format="%(asctime)s:%(levelname)s:%(message)s",
+	datefmt='[%Y/%m/%d|%H:%M:%S]'
+    )
 
+# log-like
+#print("[{}] first test".format(Path(sys.argv[0]).name))
+logging.debug("[{}] first test".format(Path(sys.argv[0]).name))
+info("hello")
 #help(pyperclip)
-print(pyperclip.__doc__)
+logging.debug(pyperclip.__doc__)
 
 pyperclip.copy("hello")
-pyperclip.paste()
-
+pyperc
