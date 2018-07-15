b00b427caf0310ab34e3cf555803024d530349e3
atrox3d <atrox3d@gmail.com>
2018-07-15 11:50:47 +0200

    start example

--- 0x06/pw.py
+++ 0x06/pw.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
-import sys
-from pathlib import Path 
-
+#
+# pw.py - An insecure password locker program.
+#
 # just to try the complete syntax
 from fakepyperclip import fakepyperclip as pyperclip
+#
+import logging
+#
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="[%(asctime)s][%(module)s][%(name)s][%(levelname)-6s] - %(message)s",
+	datefmt='%Y/%m/%d|%H:%M:%S'
+    )
+#
+log=logging.getLogger("main")
+#
+log.debug("welcome to %s", __file__)
+#
 
-# log-like
-print("[{}] first test".format(Path(sys.argv[0]).name))
-
-#help(pyperclip)
-print(pyperclip.__doc__)
-
-pyperclip.copy("hello")
-pyperclip.paste()
+PASSWORDS = {'email': 'F7minlBDDuvMJuxESSKHFhTxFtjVB6',
+             'blog': 'VmALvQyKAxiVH5G8v01if1MLZF3sdt',
+             'luggage': '12345'}
 
