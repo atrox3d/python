6921fc7cb99b47b9775d1da12bdc82e1ff179ab1
atrox3d <atrox3d@gmail.com>
2018-07-15 18:00:31 +0200

    program stub

--- 0x06.strings/0x02.addbullet/bulletPointAdder.py
+++ 0x06.strings/0x02.addbullet/bulletPointAdder.py
@@ -4,8 +4,24 @@
 #
 import sys
 
+import logging
+#
+logging.basicConfig(
+	level=logging.DEBUG,
+	#format="[%(asctime)s][%(module)s][%(name)s][%(levelname)-6s] - %(message)s",
+	format="%(asctime)s %(module)-20s %(levelname)-10s : %(message)s",
+	datefmt='%Y/%m/%d %H:%M:%S'
+	)
+#
+log=logging.getLogger(__name__)
+
 sys.path.append("..") # Adds higher directory to python modules path
 from lib.fakepyperclip import fakepyperclip as pyperclip
 
 text = pyperclip.paste()
+if text != None:
+	log.info("ok")
+else:
+	log.fatal("something went wrong, exiting")
+	sys.exit(1)
 
