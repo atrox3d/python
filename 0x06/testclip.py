2028f7b85eed1104e70512dee047172614495fd7
atrox3d <atrox3d@gmail.com>
2018-07-15 11:39:57 +0200

    loggin part 2

--- 0x06/testclip.py
+++ 0x06/testclip.py
@@ -9,16 +9,25 @@ from fakepyperclip import fakepyperclip as pyperclip
 logging.basicConfig(
     #filename="test.log",
     level=logging.DEBUG,
-    format="%(asctime)s:%(levelname)s:%(message)s",
-	datefmt='[%Y/%m/%d|%H:%M:%S]'
+    #format="%(asctime)s:%(levelname)s:%(name)s:%(message)s",
+    format="[%(asctime)s][%(module)s][%(name)s][%(levelname)-6s] - %(message)s",
+	datefmt='%Y/%m/%d|%H:%M:%S'
     )
+
+log=logging.getLogger("main")
+
 
 # log-like
 #print("[{}] first test".format(Path(sys.argv[0]).name))
-logging.debug("[{}] first test".format(Path(sys.argv[0]).name))
-info("hello")
+#log.debug("[{}] first test".format(Path(sys.argv[0]).name))
+log.info("first test")
+log.info("hello")
 #help(pyperclip)
-logging.debug(pyperclip.__doc__)
+for line in pyperclip.__doc__.split('\n'):
+	log.debug(line)
 
+log.debug('pyperclip.copy("hello")')
 pyperclip.copy("hello")
-pyperc
+log.debug('pyperclip.paste()')
+pyperclip.paste()
+
