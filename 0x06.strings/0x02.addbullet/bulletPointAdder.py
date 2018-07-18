36a4d3c1e92037709920dfaeae613ba7f49a57dc
atrox3d <atrox3d@gmail.com>
2018-07-18 18:14:51 +0200

    improve code organization

--- 0x06.strings/0x02.addbullet/bulletPointAdder.py
+++ 0x06.strings/0x02.addbullet/bulletPointAdder.py
@@ -21,26 +21,50 @@ from lib.fakepyperclip import fakepyperclip as pyperclip
 
 ##############################
 # simulate external copy
+##############################
+#
+# check if we have arguments from CLI
+#
 if len(sys.argv) > 1:
-	# from cli
+	#
+	# we have arguments, let's check them out and print them
+	#
 	log.debug("argv's len: %s", len(sys.argv))
-	log.debug(sys.argv)
-	
+	log.debug("argv      : %s", sys.argv)
+	#
+	# get lines as arguments from cli starting from 1
+	#
 	lines=sys.argv[1:]
-	log.debug(lines)
+	log.debug("lines     : %s",lines)
+	#
+	# join arguments with newline
+	#
 	pyperclip.copy('\n'.join(lines))
 else:
-	# default
+	#
+	# default:
+	#
 	pyperclip.copy('Lists of animals\nLists of aquarium life\nLists of biologists by author abbreviation\nLists of cultivars')
-
+##############################
+# /simulate external copy
 ##############################
 
-
-
+#
+# get the "copied" text
+#
 text = pyperclip.paste()
+#
+# check if we have something
+#
 if text != None:
 	log.info("ok")
+	#
+	# ok, we have data
+	#
 else:
-	log.fatal("something went wrong, exiting")
+	#
+	# no data from clipboard, exit
+	#
+	log.fatal("something went wrong, no data found in clipboard. exiting")
 	sys.exit(1)
 
