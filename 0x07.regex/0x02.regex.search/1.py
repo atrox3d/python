670cc300ef232ea0737ca3a60a62270220c7bbcf
atrox3d <atrox3d@gmail.com>
2018-07-19 15:34:22 +0200

    print exp

--- 0x07.regex/0x02.regex.search/1.py
+++ 0x07.regex/0x02.regex.search/1.py
@@ -9,12 +9,37 @@ import re
 #
 #	r'string' : raw string (no escape interpretation, otherwise \\d)
 #
-phoneNumberRegex =  re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+phoneNumberRegex =  re.compile(r'(\d\d\d)-(\d\d\d)-(\d\d\d\d)')
 #
 #	search text against regex
 #
 mo = phoneNumberRegex.search('My number is 415-555-4242.')
 #
+#	two different string interpolation methods
+#
+print("mo.group    %s" % mo.group())
+print("mo.group    {}".format(mo.group()))
+#
+#	print tuple
+#
+print("mo.groups   {}".format(mo.groups()))
+#
+#	play with tuple
+#
+for group in mo.groups():
+	print("mo.group    %5.5s" % group)
+#
+#	play with string format
+#
+for i in range(len(mo.groups())+1):
+	print("group %d	%20.20s" % (i, mo.group(i)))
+	print("group {}	{:>20s}".format( i, mo.group(i)))
+#
+# multi assignment
+#
+a, b, c = mo.groups()
+print( "a={}, b={}, c={}".format( a, b, c ))
+#
 #	print result
 #
-print('Phone number found: ' + mo.group())
+print('\nPhone number found: ' + mo.group())
