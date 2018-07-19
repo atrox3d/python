7858e8789213b396136638dde47585b3f32e24e1
atrox3d <atrox3d@gmail.com>
2018-07-19 14:57:46 +0200

    add comments

--- 0x07.regex/0x02.regex.search/1.py
+++ 0x07.regex/0x02.regex.search/1.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python3
 
+#
+#	import regex package
+#
 import re
-
+#
+#	create regex object
+#
+#	r'string' : raw string (no escape interpretation, otherwise \\d)
+#
 phoneNumberRegex =  re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
-
+#
+#	search text against regex
+#
 mo = phoneNumberRegex.search('My number is 415-555-4242.')
-
+#
+#	print result
+#
 print('Phone number found: ' + mo.group())
-
-
-
