69c7e6a859258e558292dd04861951fb44e59583
atrox3d <atrox3d@gmail.com>
2018-07-28 19:03:41 +0200

    end shelve

--- 0x08.files/shelves.py
+++ 0x08.files/shelves.py
@@ -18,4 +18,29 @@ log=logging.getLogger(__name__)
 #	using literal string interpolation (f-strings) to create variable width format
 #
 width=20
-format=f'%-{width}.{width}s : %s'
+formats=f'%-{width}.{width}s : %s'
+formatd=f'%-{width}.{width}s : %d'
+
+def writeshelf(shelf):
+	shelf['string'] = "hello there"
+	shelf['tuple'] = (1,2,3)
+	shelf['list'] = [1,2,3]
+	shelf['dict'] = { 'one' : 1, 'two' : 2}
+
+
+shelf=shelve.open('shelf')
+
+if len(shelf.items()) == 0:
+	writeshelf(shelf)
+
+log.info(formatd, 'shelf item count', len(shelf.items()) )
+
+for item in shelf.items():
+	log.info(formats,	'item', item)
+
+for key in shelf.keys():
+	log.info(formats,	'key', key)
+
+for value in shelf.values():
+	log.info(formats,	'value', value)
+
