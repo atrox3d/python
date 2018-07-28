14a5c4329a98ce80ce001715ed1e3668ca46928c
atrox3d <atrox3d@gmail.com>
2018-07-28 09:55:13 +0200

    add logger

--- 0x08.files/1.py
+++ 0x08.files/1.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
 import os, sys
+import logging
+#
+logging.basicConfig(
+	level=logging.DEBUG,
+	format="%(asctime)s %(module)-15s %(levelname)-10s : %(message)s",
+	datefmt='%Y/%m/%d %H:%M:%S'
+	)
+#
+log=logging.getLogger(__name__) 
 
 pwd = os.getcwd()
-print("current working directory : %s" % pwd)
-print("filename                  : %s" % sys.argv[0])
+log.info("current working directory : %s", pwd)
+log.info("filename                  : %s", sys.argv[0])
