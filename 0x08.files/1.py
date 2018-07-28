dd94799741da5fa13daa44126d80c179fea6af5f
atrox3d <atrox3d@gmail.com>
2018-07-28 11:35:43 +0200

    add comments

--- 0x08.files/1.py
+++ 0x08.files/1.py
@@ -2,22 +2,39 @@
 import os, sys
 import logging
 #
+#	logger configuration
+#
 logging.basicConfig(
 	level=logging.DEBUG,
 	format="%(asctime)s %(module)-15s %(levelname)-10s : %(message)s",
 	datefmt='%Y/%m/%d %H:%M:%S'
 	)
 #
+#	get a logger
+#
 log=logging.getLogger(__name__) 
-
+#
+#	where are we?
+#
 pwd = os.getcwd()
+#
+#	using literal string interpolation (f-strings) to create variable width format
+#
 width=25
 format=f'%-{width}.{width}s : %s'
+#
+#	some infos
+#
 log.info(format, "current working directory", pwd)
 log.info(format, "filename", sys.argv[0])
-
+#
+#	cwd pwd
+#
 os.chdir(pwd)
 log.info(format, "current working directory", pwd)
-
+#
+#	relative pwd
+#
 relpath=os.path.relpath(pwd)
 log.info(format, "relative working directory", relpath)
+
