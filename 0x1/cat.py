cc1cd7f24b87de54f888fa01b955482d7fbd9132
atrox3d <atrox3d@gmail.com>
2018-07-29 11:27:45 +0200

    add line numbering

--- 0x1/cat.py
+++ 0x1/cat.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
 import sys
+
+if '-n' in sys.argv[1:]:
+	lines=True
+else:
+	lines=False
+
+linenumber=0
 try:
 	for line in sys.stdin:
-		print(line.rstrip())
+		linenumber+=1
+		if lines:
+			print("%d %s" % (linenumber, line.rstrip()))
+		else:
+			print(line.rstrip())
 except KeyboardInterrupt:
 	sys.exit()
 	
