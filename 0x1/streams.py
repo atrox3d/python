d7d3db3e9ee5aefc66a1377a40b6f39bc9f3fa20
atrox3d <atrox3d@gmail.com>
2018-07-30 21:21:51 +0200

    add tests

--- 0x1/streams.py
+++ 0x1/streams.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python3
 import sys
-sys.stdout = sys.stderr
+savestdout=sys.stdout
 
+print("usage: %s 2>/dev/null" % sys.argv[0])
+
+print("this is stdout")
+sys.stdout = sys.stderr
 print("is this stderr?")
+stdoutid=id(sys.stdout)
+stderrid=id(sys.stderr)
+print("stdoutid: %s" % stderrid)
+print("stderrid: %s" % stdoutid)
+print("stdoutid == stderrid : %s" % (stdoutid == stderrid))
+
+sys.stdout=savestdout
+print("back to stdout")
+
+
 
