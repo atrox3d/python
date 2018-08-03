95eb14b43c2e80ef311158aa0ca887a9d789e39c
atrox3d <atrox3d@gmail.com>
2018-08-03 18:09:13 +0200

    functions

--- 0x1/functions.py
+++ 0x1/functions.py
@@ -10,3 +10,26 @@ logging.basicConfig(
 #
 
 log = logging.getLogger(__name__) 
+
+def getline(width=50, char="#"):
+	return char*width
+
+def logbanner( message, linewidth=50, logfunc=log.info,  char="#"):
+	logfunc(getline(linewidth,char=char))
+	logfunc(message)
+	logfunc(getline(linewidth,char))
+
+
+def afunction(aparameter="default parameter value"):
+	""" prints the parameter"""
+	print("afunction(%s)" % aparameter)
+
+log.info("afunction()	: %s", afunction.__doc__)
+afunction()
+
+
+def multiplereturn(aint=0):
+	return (aint, aint+1)
+	
+log.info("aint()	= %s", multiplereturn())
+log.info("aint()	= %d, %d", *multiplereturn())
