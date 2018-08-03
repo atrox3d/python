010fd4376bcd2d4cc4b8aa077dfec0e5c93bfa44
atrox3d <atrox3d@gmail.com>
2018-08-03 15:22:50 +0200

    add banner, fixed format log

--- 0x1/formatting.py
+++ 0x1/formatting.py
@@ -4,21 +4,40 @@ import random
 #
 logging.basicConfig(
 	level=logging.DEBUG,
-	format="%(asctime)s | %(module)-15s | %(name)-10s:%(funcName)s | %(levelname)-" + str(len("CRITICAL")) + "s | %(message)s",
+	format="%(asctime)s | %(module)-15s | %(name)-10s:%(funcName)-10s | %(levelname)-" + str(len("CRITICAL")) + "s | %(message)s",
 	datefmt='%Y/%m/%d %H:%M:%S'
 	)
 #
 log = logging.getLogger(__name__) 
 
-log.info("start")
+def getline(width=50):
+	return "#"*width
+
+def logbanner( message, linewidth=50, logfunc=log.info,  ):
+	logfunc(getline())
+	logfunc(message)
+	logfunc(getline())
+
+logbanner(
+	"FLOATS",
+	#logfunc=log.debug
+)
 
 floats = [ x * random.random() for x in range(0, 50, 5) ]
 for f in floats:
 	log.info("%-6s %%6.2f	: %6.2f", "float", f )
 
-log.info("%-6s %%-6x	: %-6x", "hex", 42 )
-log.info("%-6s %%-#6x	: %-#6x", "hex", 42 )
-log.info("%-6s %%#6x	: %#6x", "hex", 42 )
-log.info("%-6s %%#6x	: %#6x", "hex", 42 )
-log.info("%-6s %%#6X	: %#6X", "hex", 42 )
-log.info("%-6s %%#6.6x	: %#6.6x", "hex", 42 )
+logbanner(
+	"HEX",
+	#logfunc=log.debug
+)
+
+width=10
+
+log.info("%-*s %%-6x	: %-6x"  , width, "hex", 42 )
+log.info("%-*s %%-#6x	: %-#6x" , width, "hex", 42 )
+log.info("%-*s %%#6x	: %#6x"  , width, "hex", 42 )
+log.info("%-*s %%#6x	: %#6x"  , width, "hex", 42 )
+log.info("%-*s %%#6X	: %#6X"  , width, "hex", 42 )
+log.info("%-*s %%#6.6x	: %#6.6x", width, "hex", 42 )
+log.info(getline())
