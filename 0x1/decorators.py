f3f893dd51bb100919b296d186f6ad5d275290ef
atrox3d <atrox3d@gmail.com>
2018-08-05 06:50:11 +0200

    functions as args

--- 0x1/decorators.py
+++ 0x1/decorators.py
@@ -8,13 +8,25 @@ logging.basicConfig(
 	)
 log = logging.getLogger(__name__) 
 #
-#
+#	passing function as arguments
 #
 def functioncaller(f):
-	param="hello"
-	f(param)
+	print("calling " + f.__name__)
+	f()
+#
+#	define some functions
+#
+def f1():	print("f1 here")
+def f2():	print("f2 here")
+def f3():	print("f3 here")
+def f4():	print("f4 here")
+#
+#	list of functions
+#
+fx = [ f1, f2, f3, f4 ]
+#
+#	loop call functions
+#
+for f in fx:
+	functioncaller(f)
 
-def function(x):
-	print(x)
-
-functioncaller(function)
