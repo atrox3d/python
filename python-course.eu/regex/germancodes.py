04fd5f694e37943b29ca37bba681e9f4fe0c4088
atrox3d <atrox3d@gmail.com>
2018-08-07 23:40:49 +0200

    add comments

--- python-course.eu/regex/germancodes.py
+++ python-course.eu/regex/germancodes.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 import re
 from urllib.request import urlopen
-
-with urlopen('https://www.python-course.eu/post_codes_germany.txt') as gc:
-	charset=gc.info().get_content_charset()
-	for line in gc:
-		print(line.decode(charset).rstrip())
+url='https://www.python-course.eu/post_codes_germany.txt'
+with urlopen(url) as gc:						# 	open online resource
+	charset=gc.info().get_content_charset()     #	get charset
+	for line in gc:                             #	loop over content
+		print(line.decode(charset).rstrip())    #	decode every line
