35073114184da0b9243ba525f7cff493bc388ebb
atrox3d <atrox3d@gmail.com>
2018-08-07 23:41:23 +0200

    add comments

--- python-course.eu/regex/germancodes.py
+++ python-course.eu/regex/germancodes.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
+""" https://www.python-course.eu/python3_re.php """
 import re
 from urllib.request import urlopen
+
 url='https://www.python-course.eu/post_codes_germany.txt'
+
 with urlopen(url) as gc:						# 	open online resource
 	charset=gc.info().get_content_charset()     #	get charset
 	for line in gc:                             #	loop over content
