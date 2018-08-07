5d6df08786e7b6598c70670882dda9b8dbdae707
atrox3d <atrox3d@gmail.com>
2018-08-07 16:06:01 +0200

    sub packages

--- 0x1/packaging/sound/__init__.py
+++ 0x1/packaging/sound/__init__.py
@@ -6,4 +6,3 @@ This is the sound package, providing hardly anything!"""
 print("sound package is getting imported!")
 
 import sound.effects
-print("sound package is getting imported!")
--- 0x1/packaging/sound/effects/__init__.py
+++ 0x1/packaging/sound/effects/__init__.py
@@ -2,5 +2,8 @@
 
 This is the effects package, providing hardly anything!"""
 
+from .. import formats
+from ..filters import karaoke
+
 
 print("effects package is getting imported!")
