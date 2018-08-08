bfd6026de0b31f4c3ed7a54ff8bc834ce8c4877a
atrox3d <atrox3d@gmail.com>
2018-08-08 07:55:58 +0200

    parse postcodes into dict

--- python-course.eu/regex/germancodes.py
+++ python-course.eu/regex/germancodes.py
@@ -2,16 +2,59 @@
 """ https://www.python-course.eu/python3_re.php """
 import re
 from urllib.request import urlopen
+import logging
+import sys
+#
+logging.basicConfig(
+	level	= 	logging.DEBUG,
+	format	= 	"%(asctime)s | " + 
+				#"%(module)-15s | " + 
+				#"%(name)-10s:%(funcName)-10s | " + 
+				"%(levelname)-" + 
+				str(len("CRITICAL")) + "s | " + 
+				"%(message)s",
+	datefmt	= '%Y/%m/%d %H:%M:%S'
+	)
+#
+log = logging.getLogger(__name__) 
 
 url='https://www.python-course.eu/post_codes_germany.txt'
+urlfile =  url.split('/')[-1]
+with urlopen(url) as gc:							# 	open online resource
+	charset=gc.info().get_content_charset()     	#	get charset
+	
+	citycodes = {}									#	codes dict
+	
+	for line in gc:                             	#	loop over content
+		log.debug(
+					"%s : %s",
+					urlfile,
+					line.decode(charset).rstrip()	#	decode every line
+				)
+		mo = re.search(
+						r"[\d ]+"
+					#+	r"([^\d]+[a-z])"			# fails with german chars
+					+	r"([^\d\s]+)"
+					+	r"\s+"
+					+	r"(\d+)"
+					,
+					line.decode(charset).rstrip()
+				)
+				
+		if mo: 
+			log.debug("'%s', %s", mo.group(), mo.groups())
+			city, postcode = mo.groups()
+			if city in citycodes:
+				citycodes[city].add(postcode)
+			else:
+				citycodes[city] = {postcode}
 
-with urlopen(url) as gc:						# 	open online resource
-	charset=gc.info().get_content_charset()     #	get charset
-	for line in gc:                             #	loop over content
-		print(line.decode(charset).rstrip())    #	decode every line
-
-with open('german.population.txt') as gp:		# 	open online resource
-	#charset=gc.info().get_content_charset()     #	get charset
-	for line in gp:                             #	loop over content
-		print(line.rstrip())    				#	decode every line
+localfile='german.population.txt'				
+with open(localfile) as gp:							# 	open data file
+	for line in gp:                             	#	loop over content
+		log.debug(
+					"%s : %s",
+					localfile,
+					line.rstrip()					
+				)
 
