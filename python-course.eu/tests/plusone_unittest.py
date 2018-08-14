520467ac8192b472f68a050f6178f175aaa098a7
atrox3d <atrox3d@gmail.com>
2018-08-14 20:37:05 +0200

    implement setup and teardown

--- python-course.eu/tests/plusone_unittest.py
+++ python-course.eu/tests/plusone_unittest.py
@@ -1,18 +1,45 @@
 #!/usr/bin/env python3
 
 import unittest
-
+import logging
+#
+logging.basicConfig(
+	level	= 	logging.DEBUG,
+	#level	= 	logging.INFO,
+	format	= 	"%(asctime)s | " + 
+				"%(module)-15s | " + 
+				"%(name)-10s:%(funcName)-20s | " + 
+				"%(levelname)-" + 
+					str(len("CRITICAL")) + "s | " + 
+				"%(message)s",
+	datefmt	= '%Y/%m/%d %H:%M:%S'
+	) #
+log=logging.getLogger(__name__)
+#
 from plusone import plusone
 
 class PlusoneTest(unittest.TestCase):
 
-    def testCalculation(self):
-        self.assertEqual(plusone(-1 ), 0  )
-        self.assertEqual(plusone(0 ),  1 )
-        self.assertEqual(plusone(1 ),  2 )
-        self.assertEqual(plusone(5 ),  6 )
-        self.assertEqual(plusone(10),  11)
-        self.assertEqual(plusone(20),  21)
+	def setUp(self):
+		log.debug("setUp")
+		self.elems = [
+			(-1, 0),
+			( 0, 1),
+			( 1, 2),
+			( 5, 6),
+			(10, 11),
+			(20, 21),
+		]
+
+	def testCalculation(self):
+		log.debug("testCalculation")
+		for input, output in self.elems:
+			log.debug("%d == %d", plusone(input), output)
+			self.assertEqual(plusone(input), output)
+			
+	def tearDown(self):
+		log.debug("tearDown")
+		self.elems = None
 
 if __name__ == "__main__": 
     unittest.main()
