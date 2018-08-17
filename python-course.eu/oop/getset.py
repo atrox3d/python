cd62dcdda784b5e8e2201cfd175c0069931f9a52
atrox3d <atrox3d@gmail.com>
2018-08-17 21:54:02 +0200

    add getsey.py

---
+++ python-course.eu/oop/getset.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import logging
+#
+logging.basicConfig(
+	level	= 	logging.DEBUG,
+	#level	= 	logging.INFO,
+	format	= 	#"%(asctime)s | " + 
+				"%(module)s | " + 
+				#"%(name)-10s:" +
+				"%(funcName)-10s | " + 
+				"%(levelname)-" + 
+					str(len("CRITICAL")) + "s | " + 
+				"%(message)s",
+	datefmt	= '%Y/%m/%d %H:%M:%S'
+	) #
+log=logging.getLogger(__name__)
+#
+#
+#
+class P:
+	pass
+		
+		
+		
+
+if __name__ == "__main__":
+	pass
