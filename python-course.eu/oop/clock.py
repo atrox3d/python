ef07ca77238e4fa54615a923d79353e4f2f99b34
atrox3d <atrox3d@gmail.com>
2018-08-19 04:57:09 +0200

    add param check

--- python-course.eu/oop/clock.py
+++ python-course.eu/oop/clock.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-	class simulating a clock
+class simulating a clock
 """
 
 import logging
@@ -24,6 +24,9 @@ log=logging.getLogger(__name__)
 class Clock(object):
 	def __init__(self, hours, minutes, seconds):
 		"""
+		0 < hours   < 24
+		0 < minutes < 60
+		0 < seconds < 60
 		"""
 		log.debug("%-10.10s = %s", 'hours', hours)
 		log.debug("%-10.10s = %s", 'minutes', minutes)
@@ -32,10 +35,30 @@ class Clock(object):
 
 	def setclock(self, hours, minutes, seconds):
 		"""
+		0 < hours   < 24
+		0 < minutes < 60
+		0 < seconds < 60
 		"""
 		log.debug("%-10.10s = %s", 'hours', hours)
 		log.debug("%-10.10s = %s", 'minutes', minutes)
 		log.debug("%-10.10s = %s", 'seconds', seconds)
+		
+		if type(hours) == int and 0 <= hours and hours < 24:
+			self._hours = hours
+		else:
+			raise TypeError("0 < hours < 24")
+			
+		if type(minutes) == int and 0 <= minutes and minutes < 60:
+			self._minutes = minutes
+		else:
+			raise TypeError("0 < minutes < 60")
+			
+		if type(seconds) == int and 0 <= seconds and seconds < 60:
+			self._seconds = seconds
+		else:
+			raise TypeError("0 < seconds < 60")
+			
+		
 		
 	def __str__(self):
 		log.debug("%-10.10s = %s", '__str__', '__str__')
@@ -45,7 +68,7 @@ class Clock(object):
 		log.debug("%-10.10s = %s", 'tick', 'tick')
 
 if __name__ == "__main__":
-	c = Clock(1, 2, 3)
+	c = Clock(22, 59, 30)
 	print(c)
 	c.tick()
 	
