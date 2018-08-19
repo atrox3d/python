24125ebb5d7ba2922d1512e921ab25448032a89a
atrox3d <atrox3d@gmail.com>
2018-08-19 05:24:42 +0200

    add calendar.py

---
+++ python-course.eu/oop/calendar.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+class simulating a calendar
+"""
+
+import logging
+#
+logging.basicConfig(
+	level	= 	logging.DEBUG,
+	#level	= 	logging.INFO,
+	format	= 	#"%(asctime)s | " + 
+				"%(module)s | " + 
+				"%(name)-10s:" +
+				"%(funcName)-10s | " + 
+				"%(levelname)-" + 
+					str(len("CRITICAL")) + "s | " + 
+				"%(message)s",
+	datefmt	= '%Y/%m/%d %H:%M:%S'
+	) #
+log=logging.getLogger(__name__)
+#
+#
+#
+class Calendar(object):
+	pass
+
+if __name__ == "__main__":
+	pass
