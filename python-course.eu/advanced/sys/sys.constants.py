781d71ee736f32fc65d5f1dcbe59ba255b166250
atrox3d <atrox3d@gmail.com>
2018-08-21 09:54:09 +0200

    contants loop

--- python-course.eu/advanced/sys/sys.constants.py
+++ python-course.eu/advanced/sys/sys.constants.py
@@ -15,9 +15,14 @@ logging.basicConfig(
 				"%(levelname)-" + 
 					str(len("CRITICAL")) + "s | " + 
 				"%(message)s",
-	datefmt	= '%Y/%m/%d %H:%M:%S'
+	datefmt	= '%Y/%m/%d %H:%M:%S',
+	stream = sys.stdout
 	) #
 log=logging.getLogger(__name__)
 
+format = "sys.%-{0}.{0}s = %s".format(20)
 if __name__ == "__main__":
-	pass
+	for item in [x for x in dir(sys) if not x.startswith("__") or x.startswith("__std") ]:
+		log.info(format, item, getattr(sys, item))
+		#print(item)
+		
