2741f3b2f5d7f19e4d466129d3e7179b7ba79b81
atrox3d <atrox3d@gmail.com>
2018-09-17 19:45:53 +0200

    first example

---
+++ xml/parseitems.py
@@ -0,0 +1,24 @@
+#!env python3
+from xml.dom import minidom
+
+mydoc = minidom.parse('items.xml')
+
+items = mydoc.getElementsByTagName('item')
+
+print('\nitem #2 attribute')
+print(items[1].attributes['name'].value)
+
+print('\nAll attributes:')  
+for elem in items:
+	print(elem.attributes['name'].value)
+	#print(dir(elem))
+		
+print('\nItem #2 data')
+print(items[1].firstChild.data)
+print(items[1].childNodes[0].data)
+
+print('\nAll item data:')  
+for elem in items:
+	print(elem.firstChild.data)
+	
+
