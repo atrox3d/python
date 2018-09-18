9e2e47afb29d73e0629e3eab5bc78931402b5ba0
atrox3d <atrox3d@gmail.com>
2018-09-18 07:07:56 +0200

    add some comments

--- xml/parseitems.py
+++ xml/parseitems.py
@@ -1,13 +1,17 @@
 #!env python3
-from xml.dom import minidom
+
 import sys
 
+from xml.dom import minidom
 
+# get path to xml via args or use default
 datafile = sys.argv[1] if len(sys.argv) > 1 else 'items.xml'
 
+# open and parse datafile
 datasource = open(datafile)
 mydoc = minidom.parse(datasource)
 
+# search item tags
 items = mydoc.getElementsByTagName('item')
 
 print('\nitem #2 attribute')
@@ -16,7 +20,6 @@ print(items[1].attributes['name'].value)
 print('\nAll attributes:')  
 for elem in items:
 	print(elem.attributes['name'].value)
-	#print(dir(elem))
 		
 print('\nItem #2 data')
 print(items[1].firstChild.data)
