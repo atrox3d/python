42d72f717bbd8e46e8de31a62a1d5ac6bf174dfd
atrox3d <atrox3d@gmail.com>
2018-09-18 08:01:45 +0200

    some test

--- xml/elementree.py
+++ xml/elementree.py
@@ -7,6 +7,11 @@ from xml.etree import ElementTree as ET
 tree = ET.parse('items.xml')
 root = tree.getroot()
 
+#print('root', root)
+#print('root public members  : ', list([ p for p in dir(root) if not p.startswith('__')]))
+#print('root private members : ', dir(root))
+#print(root.__getitem__(0))
+
 # one specific item attribute
 print('item #2 attribute')
 print(root[0][1].attrib)
