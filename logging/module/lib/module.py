3716dfede54ee0b3723bcd94912cfc63b20a8c37
atrox3d <atrox3d@gmail.com>
2018-09-23 15:25:18 +0200

    add module log file

--- logging/module/lib/module.py
+++ logging/module/lib/module.py
@@ -4,8 +4,29 @@
 
 import logging
 
+# create console handler and set level to DEBUG
+fh = logging.FileHandler('module.log', mode='w')
+fh.setLevel(logging.DEBUG)
+
+# create formatter
+moduleFileFormatter = logging.Formatter(
+									  '[moduleFileFormatter] '
+									+ '%(asctime)s'
+									+ ' - '
+									+ '%(name)s'		# simple_example
+									+ ' - '
+									+ '%(levelname)s'
+									+ ' - '
+									+ '%(message)s'
+							)
+
+fh.setFormatter(moduleFileFormatter)
+
 # create logger
 modulelogger = logging.getLogger('main.module')
+
+modulelogger.addHandler(fh)
+#modulelogger.propagate=False
 
 modulelogger.info		(	'%s::loading START ***',	__name__)
 
