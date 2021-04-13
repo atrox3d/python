2c6f1961336373e53a7f8c9be5b0e300e93e3ba2
atrox3d <atrox3d@gmail.com>
2021-04-13 13:55:45 +0200

    moved python-course.eu ..

--- python-course.eu/packaging/simple_package/__init__.py
+++
@@ -1,7 +0,0 @@
-import simple_package.a
-print( "simple_package.a loaded" )
-import simple_package.b
-print( "simple_package.b loaded" )
-
-a.bar()
-b.foo()
--- python-course.eu/packaging/simple_package/b.py
+++
@@ -1,2 +0,0 @@
-def foo():
-    print("Hello, function 'foo' from module 'b' calling")
